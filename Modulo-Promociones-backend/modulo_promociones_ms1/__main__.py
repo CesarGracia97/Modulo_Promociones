cd447ae1eafcaae7c3163e244f043649118f9d5f
Cesar Gracia <cesar.gracia@pucese.edu.ec>
2024-03-14 17:11:11 -0500

    Resuelto el problema de requirements

---
+++ Modulo-Promociones-backend/modulo_promociones_ms1/__main__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from Resources.database.connection import connection
+from Controllers.PeticionPlacesController import receptor_bplc
+from Controllers.PeticionPlanesController import receptor_bpln
+
+
+def create_app():
+    # Crea la aplicación Flask
+    app = Flask(__name__)
+
+    # Configuración
+    # app.config.from_pyfile('config.py')  # Considera tener un archivo de configuración separado
+
+    # Conexión a la base de datos
+    oracle_db = connection()
+    if oracle_db.connect():
+        # Registro de blueprints
+        app.register_blueprint(receptor_bplc)
+        app.register_blueprint(receptor_bpln)
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(port=5011, debug="FALSE")
