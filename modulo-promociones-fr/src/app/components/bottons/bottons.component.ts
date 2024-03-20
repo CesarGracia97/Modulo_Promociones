24e214ef8b4de4ccb41d833f48daacdfd55c283c
Cesar Gracia <cesar.gracia@pucese.edu.ec>
2024-03-20 12:08:26 -0500

    Front - Botones sidbar

--- modulo-promociones-fr/src/app/components/bottons/bottons.component.ts
+++ modulo-promociones-fr/src/app/components/bottons/bottons.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-bottons',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './bottons.component.html',
   styleUrl: './bottons.component.scss'
 })
-export class BottonsComponent {
+export class BottonsComponent implements OnInit {
+  options = [
+    {name: 'Tipo de Servicio', value:'opcion-1'},
+    {name: 'Red', value:'opcion-2'},
+    {name: 'Plan', value:'opcion-3'},
+    {name: 'Provincial', value:'opcion-4'},
+    {name: 'Ciudad', value:'opcion-5'},
+    {name: 'Sector', value:'opcion-6'}
+  ]
 
+  constructor(){ }
+
+  ngOnInit(): void {}
 }
