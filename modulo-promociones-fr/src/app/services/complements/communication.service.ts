8d493930acd610ac3c9fe617d4aade14a27991ed
Cesar Gracia <cesar.gracia@pucese.edu.ec>
2024-04-01 16:59:11 -0500

    Front-end: falla en comunicacion

--- modulo-promociones-fr/src/app/services/complements/communication.service.ts
+++ modulo-promociones-fr/src/app/services/complements/communication.service.ts
@@ -12,6 +12,7 @@ export class CommunicationService {
   constructor() {}
 
   sendSelectedButton(buttonId: string) {
+    console.log("CommunicationService - Activo | Valor:", buttonId)
     this.selectedButtonSubject.next(buttonId);
   }
 }
