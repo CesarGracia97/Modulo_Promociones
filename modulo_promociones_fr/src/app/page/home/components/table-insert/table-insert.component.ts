b495b424e50a2bd02351e4461f7e87d4ac47dbfb
Cesar Gracia <cesar.gracia@pucese.edu.ec>
2024-04-18 17:09:53 -0500

    Front-end: Agregar datos, estilo

--- modulo_promociones_fr/src/app/page/home/components/table-insert/table-insert.component.ts
+++ modulo_promociones_fr/src/app/page/home/components/table-insert/table-insert.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-table-insert',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './table-insert.component.html',
   styleUrl: './table-insert.component.scss'
 })
-export class TableInsertComponent {
+export class TableInsertComponent implements OnInit  {  
 
+  rows: any[] = [];
+
+  ngOnInit(): void {
+    this.addRow(); // Añade la primera fila automáticamente al cargar
+  }
+
+  addRow(): void {
+    const newRow = {
+      id: this.rows.length + 1, // El ID será el contador de filas
+      servicio: '',
+      tipoServicio: '',
+      red: '',
+      plan: '',
+      formaPago: '',
+      buro: '',
+      provincia: '',
+      ciudad: '',
+      sector: ''
+    };
+    this.rows.push(newRow); // Añade el nuevo objeto al array de filas
+  }
 }
