9a24d4a96d0956eefce9dcb69a4c73636c401277
Cesar Gracia <cesar.gracia@pucese.edu.ec>
2024-04-26 10:47:08 -0500

    Front-end: Checklist

---
+++ ejecucion_back.bat
@@ -0,0 +1,9 @@
+@echo off
+cd C:\Users\cgracia\source\proyectos\Modulo-Promociones\modulo_promociones_ms1
+start python __main__.py
+cd C:\Users\cgracia\source\proyectos\Modulo-Promociones\modulo_promociones_rapi1
+start python __main__.py
+cd C:\Users\cgracia\source\proyectos\Modulo-Promociones\modulo_promociones_rapi2
+start python __main__.py
+cd C:\Users\cgracia\source\proyectos\Modulo-Promociones\modulo_promociones_rapi3
+start python __main__.py
