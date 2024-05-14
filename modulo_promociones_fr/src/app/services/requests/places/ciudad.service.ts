394e390020723fb82bab986bc5fb073546154f99
Cesar Gracia <cesar.gracia@pucese.edu.ec>
2024-05-14 14:36:30 -0500

    Front-end: Adaptaciones del back en los servicios

--- modulo_promociones_fr/src/app/services/requests/places/ciudad.service.ts
+++ modulo_promociones_fr/src/app/services/requests/places/ciudad.service.ts
@@ -8,33 +8,33 @@ import { Ciudades } from '../../../interfaces/places/ciudad.interface';
 })
 export class CiudadService {
 
-  private UrlSimple = 'http://127.0.0.1:5012/api/ra/plccity_endpoint';
-  private UrlMasive = 'http://127.0.0.1:5012/api/ra/plcinfomasiva_endpoint';
+  private URLSimple = 'http://127.0.0.1:5012/api/ra/plccity_endpoint';
+  private URLMasive = 'http://127.0.0.1:5012/api/ra/plcinfomasiva_endpoint';
 
   constructor(private http:HttpClient) { }
 
   getCiudadesALL(): Observable<Ciudades[]>{
     let params = new HttpParams().set('type', 'ALL_CITIES');
-    return this.http.get<Ciudades[]>(this.UrlSimple, { params: params });
+    return this.http.get<Ciudades[]>(this.URLSimple, { params: params });
   }
 
   getCiudadesESP(id_Prov:number):Observable<Ciudades[]>{
     let params = new HttpParams().set('type', 'CIUDADES_ESPECIFICASxPROV')
     .set('id_Prov', id_Prov.toString());
-    return this.http.get<Ciudades[]>(this.UrlSimple, { params: params });
+    return this.http.get<Ciudades[]>(this.URLSimple, { params: params });
   }
 
   getCiudadesALLXTariffplanVariant(tariffplanvariant: number): Observable <Ciudades[]> {
     let params = new HttpParams().set('type', 'CIUDADES_ESPECIFICASxTFV')
     .set('TARIFFPLANVARIANT', tariffplanvariant.toString());
-    return this.http.get<Ciudades[]>(this.UrlSimple, { params: params });
+    return this.http.get<Ciudades[]>(this.URLSimple, { params: params });
   }
 
   getCiudadesXTariffplanVariant(id_Prov:number, tariffplanvariant: number):Observable<Ciudades[]>{
     let params = new HttpParams().set('type', 'CIUDADES_ESPECIFICASxPROVxTFV')
     .set('id_Prov', id_Prov.toString())
     .set('TARIFFPLANVARIANT', tariffplanvariant);
-    return this.http.get<Ciudades[]>(this.UrlSimple, { params: params });
+    return this.http.get<Ciudades[]>(this.URLSimple, { params: params });
   }
 
   getCiudadesMasivasXTariffplanVariant(id_Prov: number[], tariffplanvariant: number): Observable <Ciudades[]> {
@@ -42,6 +42,6 @@ export class CiudadService {
     let params = new HttpParams().set('type', 'CIUDADES_ESPECIFICASxPROVxTFV')
     .set('id_Provs', a_idProv)
     .set('TARIFFPLANVARIANT', tariffplanvariant.toString());
-    return this.http.get<Ciudades[]>(this.UrlMasive, { params: params });
+    return this.http.get<Ciudades[]>(this.URLMasive, { params: params });
   }
 }
